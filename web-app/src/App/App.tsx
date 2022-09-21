85cba07bc91923f2dfb08943f899910adb923fdb
Adam Roux <roux.adam69@gmail.com>
2022-09-21 15:54:26 +0200

    setup typescript

--- web-app/src/App/App.tsx
+++ web-app/src/App/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import CreateWilder from "../pages/CreateWilder/CreateWilder";
 import Home from "../pages/Home/Home";
--- web-app/src/index.tsx
+++ web-app/src/index.tsx
@@ -7,7 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
