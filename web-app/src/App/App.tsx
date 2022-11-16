61f84e2a4418ed716f66cabf6806596a3ec76510
Adam Roux <roux.adam69@gmail.com>
2022-11-16 16:21:44 +0100

    add sign-up and sign-in methods

--- web-app/src/App/App.tsx
+++ web-app/src/App/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
 import CreateWilder from '../pages/CreateWilder/CreateWilder';
 import Home from '../pages/Home/Home';
-import { CREATE_WILDER_PATH, HOME_PATH } from '../pages/paths';
+import { CREATE_WILDER_PATH, HOME_PATH, SIGN_IN_PATH, SIGN_UP_PATH } from '../pages/paths';
+import SignIn from '../pages/SignIn/SignIn';
+import SignUp from '../pages/SignUp/SignUp';
 import { Container, Footer, Header, MainContainer, PageTitle, PageTitleLink } from './App.styled';
 
 function App() {
@@ -15,12 +17,18 @@ function App() {
           <PageTitle>
             <PageTitleLink to={HOME_PATH}>Wilders Book</PageTitleLink>
           </PageTitle>
+          <nav>
+            <Link to="/sign-up">S'inscrire</Link> |
+            <Link to="/sign-in">Se connecter</Link>
+          </nav>
         </Container>
       </Header>
       <MainContainer>
         <Routes>
           <Route path={HOME_PATH} element={<Home />} />
           <Route path={CREATE_WILDER_PATH} element={<CreateWilder />} />
+          <Route path={SIGN_IN_PATH} element={<SignIn />} />
+          <Route path={SIGN_UP_PATH} element={<SignUp />} />
         </Routes>
       </MainContainer>
       <Footer>
